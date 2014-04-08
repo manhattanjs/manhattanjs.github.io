bdd95e2a12f78822d713b9f94b0bb6fee5219697
Brenda Storer <brendastorer@gmail.com>
2014-04-08 16:41:11 -0400

    Float the clouds according to browser scroll in the logo.

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,20 @@
-$(window).scroll(function() {
-  $('.js-scrolling-clouds').each(function(){
-    $(this).addClass("js-ease-out-right");
+// Float the Clouds
+
+$(document).ready(function(){
+  var windowTop = $(window).scrollTop(),
+      windowHeight = $(window).height()
+
+  $(window).scroll(function(){
+    windowTop = $(window).scrollTop();
+
+    $('.js-cloud-1').css({
+      'left': (windowTop) * .3 + 'px'
+    });
+
+    $('.js-cloud-2').css({
+      'left': (windowTop) * .7 + 'px'
+    });
+
   });
+
 });
