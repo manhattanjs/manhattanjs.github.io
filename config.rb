2c537ce557eb7a662907db5b9203821a27011813
Brenda Storer <brendastorer@gmail.com>
2014-04-29 09:11:16 -0400

    Clean up of file organization, stylesheets.

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "assets/stylesheets"
+sass_dir = "assets/stylesheets/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
