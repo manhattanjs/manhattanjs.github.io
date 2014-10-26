65ea11bad94d6eded5ce3cbff2d0bae49249fe9c
Brenda Storer <brendastorer@gmail.com>
2014-10-25 23:25:47 -0400

    Update js to use css translate for smoother animation

--- assets/javascripts/animations.js
+++ assets/javascripts/animations.js
@@ -6,13 +6,11 @@ $(document).ready(function(){
     windowTop = $(window).scrollTop();
 
     $('.js-cloud-1').css({
-      'left': (windowTop) * .3 + 'px'
+      'transform':'translateX('+(windowTop) * .3 +'px)'
     });
 
     $('.js-cloud-2').css({
-      'left': (windowTop) * .7 + 'px'
+      'transform':'translateX('+(windowTop) * .7 +'px)'
     });
-
   });
-
 });
