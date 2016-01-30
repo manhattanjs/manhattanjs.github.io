b625982482e92994f39afbea2bb79e0b863fb3a3
HipsterBrown <headhipster@hipsterbrown.com>
2016-01-30 14:44:59 -0500

    refactor
    Includes:
    
    - content fixes
    - image compression
    - removes extra javascript/jQuery
    - sponsor page styling

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,47 +1,49 @@
-$(document).ready(function() {
-	mixpanel.track(
-		"Viewed Homepage"
-	);
+mixpanel.track(
+  "Viewed Homepage"
+);
 
-	function getTimeRemaining(endtime) {
-	  var t = Date.parse(endtime) - Date.parse(new Date());
-	  var seconds = Math.floor((t / 1000) % 60);
-	  var minutes = Math.floor((t / 1000 / 60) % 60);
-	  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-	  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-	  return {
-	    'total': t,
-	    'days': days,
-	    'hours': hours,
-	    'minutes': minutes,
-	    'seconds': seconds
-	  };
-	}
+function isComplete(time) {
+  return time <= 0;
+}
 
-	function initializeClock(id, endtime) {
-	  var clock = document.getElementById(id);
-	  var daysSpan = clock.querySelector('.days');
-	  var hoursSpan = clock.querySelector('.hours');
-	  var minutesSpan = clock.querySelector('.minutes');
-	  var secondsSpan = clock.querySelector('.seconds');
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': isComplete(t) ? 0 : days,
+    'hours': isComplete(t) ? 0 : hours,
+    'minutes': isComplete(t) ? 0 : minutes,
+    'seconds': isComplete(t) ? 0 : seconds
+  };
+}
 
-	  function updateClock() {
-	    var t = getTimeRemaining(endtime);
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
 
-	    daysSpan.innerHTML = t.days;
-	    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-	    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-	    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
 
-	    if (t.total <= 0) {
-	      clearInterval(timeinterval);
-	    }
-	  }
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-	  updateClock();
-	  var timeinterval = setInterval(updateClock, 1000);
-	}
+    if (isComplete(t.total)) {
+      clearInterval(timeinterval);
+    }
+  }
 
-	var deadline = 'February 10 2016 18:59:59 GMT-05:00';
-	initializeClock('clockdiv', deadline);
-});
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = 'February 10 2016 18:59:59 GMT-05:00';
+initializeClock('clockdiv', deadline);
