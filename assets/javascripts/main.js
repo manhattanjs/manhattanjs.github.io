cc45e79687c1becff4ea5aace749b9aa1c86ea35
Nick Hehr <headhipster@hipsterbrown.com>
2016-06-21 13:49:47 -0400

    quick JS fix for sponsorship page

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -23,6 +23,10 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
+  if (!clock) {
+    return;
+  }
+
   var daysSpan = clock.querySelector('.days');
   var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
