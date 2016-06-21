d7d3a8e1f5d2e3572a78c63daedf885035527ec5
Nick Hehr <headhipster@hipsterbrown.com>
2016-06-21 13:37:36 -0400

    updates details for July

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -45,5 +45,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = 'June 08 2016 17:59:59 GMT-05:00';
+var deadline = 'July 13 2016 17:59:59 GMT-05:00';
 initializeClock('clockdiv', deadline);
