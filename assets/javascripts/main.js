ae223fe7cca97b49dc53d18598f2f39959f20763
Rushaine McBean <rushaine.mcbean@gmail.com>
2017-05-20 12:49:13 -0400

    Updates for June 2017 meetup

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -45,5 +45,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = 'May 10   2017 17:59:59 GMT-05:00';
+var deadline = 'June 14   2017 17:59:59 GMT-05:00';
 initializeClock('clockdiv', deadline);
