8cf6e486b0c5f4761ff1f048824f79752c0eeb16
Rushaine McBean <rushaine.mcbean@gmail.com>
2018-07-28 11:18:32 -0400

    Updates for August 2018 meetup

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -45,5 +45,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = 'July 11 2018 17:59:59 GMT-05:00';
+var deadline = 'August 08 2018 17:59:59 GMT-05:00';
 initializeClock('clockdiv', deadline);
